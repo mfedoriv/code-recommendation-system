950f909355ac70d047645490ade4c094c791e5e4
mfedoriv <mfedorivs@gmail.com>
2020-03-26 21:00:05 +0300

    Added method Parser.parserGithub. Now it's just download JSON data from Github, but don't return any valid result.\nAdded ParseException

--- src/main/java/org/suai/parser/Parser.java
+++ src/main/java/org/suai/parser/Parser.java
@@ -1,10 +1,15 @@
 package org.suai.parser;
 
-import com.jcabi.github.Github;
-import com.jcabi.github.RtGithub;
+//import com.jcabi.github.Content;
+//import com.jcabi.github.Github;
+//import com.jcabi.github.RtGithub;
+//import com.jcabi.github.Search;
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.suai.Example;
 
 import java.io.*;
+import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
@@ -60,7 +65,7 @@ public class Parser {
         int stringCounter = 0;
         try {
             is = url.openStream();  // throws an IOException
-//            fis = new FileInputStream("D:\\shared\\JavaPGM\\4thYear\\code-recommendation-system\\code-recommendation-system\\cplusplus_fgets.html");
+//            fis = new FileInputStream("cplusplus_fgets.html");
             br = new BufferedReader(new InputStreamReader(is, StandardCharsets.UTF_8));
 //            br = new BufferedReader(new InputStreamReader(fis, StandardCharsets.UTF_8));
 
@@ -105,7 +110,77 @@ public class Parser {
         return new Example(out.toString());
     }
 
-//    public static Example parserGithub(String funcName) {
-//        Github github = new RtGithub();
-//    }
+    public static Example parserGithub(String funcName) throws ParseException {
+        String oauthToken = null;
+        try(BufferedReader br = new BufferedReader(new FileReader("src/main/java/org/suai/github_token.txt"))) {
+            for(String line; (line = br.readLine()) != null; ) {
+                line = line.trim();
+                if (line.charAt(0) == '#') {
+                    continue;
+                }
+                oauthToken = line;
+                break;
+            }
+        } catch (IOException e) {
+            throw new ParseException(e.getMessage());
+        }
+        if (oauthToken == null) {
+            throw new ParseException("Can't find your OAuth GitHub Token! Please insert it in file github_token.txt");
+        }
+//        https://api.github.com/search/code?q={query}{&page,per_page,sort,order}
+        HttpURLConnection connection = null;
+        BufferedReader reader;
+        String line;
+        StringBuffer responseContent = new StringBuffer();
+        try {
+            URL url = new URL("https://api.github.com/search/code?q=" + funcName + "+in:file+language:C++");
+//            URL url = new URL("https://api.github.com");
+//            URL url = new URL("https://jsonplaceholder.typicode.com/posts");
+            connection = (HttpURLConnection) url.openConnection();
+
+            connection.setRequestMethod("GET");
+            connection.setConnectTimeout(5000);
+            connection.setRequestProperty("Authorization", "token " + oauthToken);
+            int status = connection.getResponseCode();
+            if (status > 299) {
+                reader = new BufferedReader(new InputStreamReader(connection.getErrorStream()));
+                while((line = reader.readLine()) != null) {
+                    responseContent.append(line);
+                }
+                reader.close();
+            } else {
+                reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                while((line = reader.readLine()) != null) {
+                    responseContent.append(line);
+                }
+                reader.close();
+            }
+            JSONObject jo = new JSONObject(responseContent.toString());
+            JSONArray items = jo.getJSONArray("items");
+            for (int i = 0; i < items.length(); i++) {
+                System.out.println(items.get(i));
+            }
+
+
+        } catch (IOException e) {
+            throw new ParseException(e.getMessage());
+        } finally {
+            if (connection != null) {
+                connection.disconnect();
+            }
+        }
+
+//        Github github = new RtGithub(oauthToken);
+//        Iterable<Content> list = null;
+//        try {
+//            list = github.search().codes(funcName + "language:C++", "best match", Search.Order.DESC);
+//        } catch (IOException e) {
+//            throw new ParseException(e.getMessage());
+//        }
+//        for (Content o: list) {
+//            System.out.println(o.toString());
+//        }
+        return null;
+    }
+
 }
--- src/main/java/org/suai/test.java
+++ src/main/java/org/suai/test.java
@@ -1,7 +1,16 @@
 package org.suai;
 
+import org.suai.parser.ParseException;
+import org.suai.parser.Parser;
+
 public class test {
     public static void main(String[] args) {
 //        System.out.println(Parser.parseData());
+        Example example;
+        try {
+            example = Parser.parserGithub("fopen");
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
     }
 }
